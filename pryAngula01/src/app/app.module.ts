4ce2e792aab2c67b9ad5f2fd46ad71d6359549f9
darlanona <darlan_ona@hotmail.com>
2022-09-01 15:02:47 -0500

    primer codigo angular

--- pryAngula01/src/app/app.module.ts
+++ pryAngula01/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente01Component } from './componente01/componente01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente01Component
   ],
   imports: [
     BrowserModule,
