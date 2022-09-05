7941841c1832843f85595785d7d87f9d0ca263f3
darlanona <darlan_ona@hotmail.com>
2022-09-05 14:58:33 -0500

    creacion componente

--- pryAngula01/src/app/app.module.ts
+++ pryAngula01/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { Componente01Component } from './componente01/componente01.component';
 import {ButtonModule} from 'primeng/button';
 import {MultiSelectModule} from 'primeng/multiselect';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Componente02Component } from './modulo01/componente02.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Componente01Component
+    Componente01Component,
+    Componente02Component
   ],
   imports: [
     BrowserModule,
---
+++ pryAngula01/src/app/modulo01/modulo01.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Componente02Component } from './componente02/componente02.component';
+
+
+
+@NgModule({
+  declarations: [
+    Componente02Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Modulo01Module { }
